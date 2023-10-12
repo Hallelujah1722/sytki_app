e3b7826b83fb8ba07b01ef9ca3f033ea6c97b4a6
emir <imir.guseinov2003@mail.ru>
2023-10-12 13:53:57 +0300

    Подключил PostgreSQL к главному коду, для удобства убрал в файцл db_connect

---
+++ db_connect.py
@@ -0,0 +1,29 @@
+import psycopg2
+from psycopg2 import Error #обработчик ошибок
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from flask_sqlalchemy import SQLAlchemy
+
+try:
+    #Подключение к бд PostgeSQL(пока что на локальном сервере)
+    connection = psycopg2.connect(user = "emir",
+                                  database = "sytki",
+                                  password = "password",
+                                  host = "127.0.0.1",
+                                  port = "5432")
+
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    #создаю курсор
+    cursor = connection.cursor()
+    print("Подключение к PostgreSQL")
+    print(connection.get_dsn_parameters(), "\n")
+    print("Вы подключены к - PostgreSQL 16.0 ", "\n")
+    cursor.execute("SELECT datname FROM pg_database;")
+    print(cursor.fetchall())
+
+except (Exception, Error) as error:
+    print("Ошибка при работе с PostgreSQL", error)
+finally:
+    if connection:
+        cursor.close()
+        connection.close()
+        print("Соединение с PostgreSQL закрыто")
