4e2a1fd36f741112acfca71898da0f078d25ce62
emir <imir.guseinov2003@mail.ru>
2023-10-12 23:31:09 +0300

    вывел данные в консоль из анкеты

--- db_connect.py
+++ db_connect.py
@@ -6,22 +6,23 @@ from flask_sqlalchemy import SQLAlchemy
 try:
     #Подключение к бд PostgeSQL(пока что на локальном сервере)
     connection = psycopg2.connect(user = "emir",
-                                  database = "sytki",
+                                  dbname = "sytki",
                                   password = "password",
                                   host = "127.0.0.1",
                                   port = "5432")
 
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT) #кажется это автосохранение измененный данных)
     #создаю курсор
     cursor = connection.cursor()
     print("Подключение к PostgreSQL")
     print(connection.get_dsn_parameters(), "\n")
     print("Вы подключены к - PostgreSQL 16.0 ", "\n")
     cursor.execute("SELECT datname FROM pg_database;")
+    cursor.execute("SELECT * FROM kyrsants;")
     print(cursor.fetchall())
 
 except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
+    print("Ошибка при работе с PostgreSQL", error) #вывод ошибок
 finally:
     if connection:
         cursor.close()
